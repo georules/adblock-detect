fa1ab24031969a480e25d83a43043ebb125f1da1
Geo Miller <geoffery.miller@gmail.com>
2013-11-08 16:19:38 -0500

    omg so dirty

--- adblock-detect.js
+++ adblock-detect.js
@@ -1,7 +1,31 @@
 (function(window, undefined) {
+
+
+var checkAds = function(userCallback) {
+  window.adblocked.userCallback = userCallback
+  var a = new Adblocked()
+  a.insert()
+}
+
+var done = function() {
+  if (typeof(window.google_ad_block) === "undefined")  {
+    window.adblocked.result = true
+  }
+  else {
+    window.adblocked.result = false
+  }
+  window.adblocked.userCallback()
+}
+
+window.adblocked = {
+  check : checkAds,
+  userCallback : function() {},
+  callback : done,
+  result : "unknown"
+}
+
 var Adblocked = function() {
 
-this.result = "unknown";
 this.scriptFile = "http://pagead2.googlesyndication.com/pagead/show_ads.js";
 this.adScript = document.createElement("script")
 
@@ -12,29 +36,16 @@ this.insert = function() {
   this.adScript.setAttribute("type","text/javascript")
   this.adScript.setAttribute("src",this.scriptFile)
   body.appendChild(ad).appendChild(this.adScript)
-
-  return this
-};
-
-this.check = function() {
-  if (typeof(window.google_ad_block) !== "undefined")  {
-    this.result = false
-  }
-  else {
-    this.result = true
-  }
+  this.adScript.onload = adblocked.callback;
   return this
 };
 
 return this;
+
 }
 
 var a = new Adblocked()
-a.insert().check()
-
-window.adblocked = function() {
-  return a.result;
-}
+a.insert()
 
 })(window)
 
