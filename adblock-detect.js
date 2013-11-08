63d4ced02825d1be2e6c574c0bddb35a34cb2b3b
Geo Miller <geoffery.miller@gmail.com>
2013-11-08 15:32:04 -0500

    five alive

--- adblock-detect.js
+++ adblock-detect.js
@@ -2,24 +2,27 @@
 var Adblocked = function() {
 
 this.result = "unknown";
-//this.scriptFile = "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.js";
 this.scriptFile = "http://pagead2.googlesyndication.com/pagead/show_ads.js";
+this.adScript = document.createElement("script")
 
 this.insert = function() {
   var body = document.getElementsByTagName('body')[0]
   var ad = document.createElement("div")
   ad.style.display = "none";
-  var adScript = document.createElement("script")
-  adScript.setAttribute("type","text/javascript")
-  adScript.setAttribute("src",this.scriptFile)
-
-  body.appendChild(ad).appendChild(adScript)
+  this.adScript.setAttribute("type","text/javascript")
+  this.adScript.setAttribute("src",this.scriptFile)
+  body.appendChild(ad).appendChild(this.adScript)
 
   return this
 };
 
 this.check = function() {
-  this.result = true
+  if (typeof(window.google_ad_block) !== null)  {
+    this.result = false
+  }
+  else {
+    this.result = true
+  }
   return this
 };
 
