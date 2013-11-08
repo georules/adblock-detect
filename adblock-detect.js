ce7c9f22ff7036bd39a59f62dcff7f5cef1d128b
Geo Miller <geoffery.miller@gmail.com>
2013-11-08 15:20:50 -0500

    test some x

--- adblock-detect.js
+++ adblock-detect.js
@@ -1,34 +1,37 @@
 (function(window, undefined) {
-var adblocked = function() {
-  function adblocked() {
-    this.result = "unknown";
-    this.scriptFile = "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.js";
-  }  
+var Adblocked = function() {
 
-  this.insert = function() {
-    var body = document.getElementsByTagName('body')[0]
-    var ad = document.createElement("div")
-    ad.style.display = "none";
-    var adScript = document.createElement("script")
-    adScript.setAttribute("type","text/javascript")
-    adScript.setAttribute("src",this.scriptFile)
+this.result = "unknown";
+//this.scriptFile = "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.js";
+this.scriptFile = "http://pagead2.googlesyndication.com/pagead/show_ads.js";
 
-    body.appendChild(ad).appendChild(adScript)
+this.insert = function() {
+  var body = document.getElementsByTagName('body')[0]
+  var ad = document.createElement("div")
+  ad.style.display = "none";
+  var adScript = document.createElement("script")
+  adScript.setAttribute("type","text/javascript")
+  adScript.setAttribute("src",this.scriptFile)
 
-    return this
-  };
+  body.appendChild(ad).appendChild(adScript)
 
-  this.check = function() {
-    this.result = true
-    return this
-  };
+  return this
+};
 
-  window.adblocked = function() {
-    return this.result
-  }
+this.check = function() {
+  this.result = true
+  return this
+};
+
+return this;
 }
 
-new adblocked().insert().check()
+var a = new Adblocked()
+a.insert().check()
+
+window.adblocked = function() {
+  return a.result;
+}
 
 })(window)
 
