f611f72fe618aa5aea14d5f86a6e7fd2dcd88a61
Geo Miller <geoffery.miller@gmail.com>
2013-11-10 15:24:28 -0500

    some cleanup

--- adblock-detect.js
+++ adblock-detect.js
@@ -1,13 +1,11 @@
 (function(window, undefined) {
 
+var Adblocked = function() {
 
-var checkAds = function(userCallback) {
-  window.adblocked.userCallback = userCallback
-  var a = new Adblocked()
-  a.insert()
-}
+this.scriptFile = "http://pagead2.googlesyndication.com/pagead/show_ads.js";
+this.adScript = document.createElement("script")
 
-var done = function() {
+this.done = function() {
   if (typeof(window.google_ad_block) === "undefined")  {
     window.adblocked.result = true
   }
@@ -17,18 +15,6 @@ var done = function() {
   window.adblocked.userCallback()
 }
 
-window.adblocked = {
-  check : checkAds,
-  userCallback : function() {},
-  callback : done,
-  result : "unknown"
-}
-
-var Adblocked = function() {
-
-this.scriptFile = "http://pagead2.googlesyndication.com/pagead/show_ads.js";
-this.adScript = document.createElement("script")
-
 this.insert = function() {
   var body = document.getElementsByTagName('body')[0]
   var ad = document.createElement("div")
@@ -36,8 +22,8 @@ this.insert = function() {
   this.adScript.setAttribute("type","text/javascript")
   this.adScript.setAttribute("src",this.scriptFile)
   body.appendChild(ad).appendChild(this.adScript)
-  this.adScript.onload = adblocked.callback;
-  this.adScript.onerror = adblocked.callback;
+  this.adScript.onload = this.done;
+  this.adScript.onerror = this.done;
 
   return this
 };
@@ -46,8 +32,17 @@ return this;
 
 }
 
-var a = new Adblocked()
-a.insert()
+var checkAds = function(userCallback) {
+  window.adblocked.userCallback = userCallback
+  var a = new Adblocked()
+  a.insert()
+}
+
+window.adblocked = {
+  check : checkAds,
+  userCallback : function() {},
+  result : "unknown"
+}
 
 })(window)
 
